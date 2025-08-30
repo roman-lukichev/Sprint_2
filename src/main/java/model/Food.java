63a9b57404e55fac71c19c986d747f0574030d65
Roman Lukichev <lukichev.official@gmail.com>
2025-08-30 16:51:01 +0300

    refactor: сделать конструктор Food protected вместо public

--- src/main/java/model/Food.java
+++ src/main/java/model/Food.java
@@ -7,7 +7,7 @@ public abstract class Food implements Discountable{
     protected double price; //цена за килограмм
     protected boolean isVegetarian; //вегетарианский ли продукт
 
-    public Food(int amount, double price, boolean isVegetarian) {
+    protected Food(int amount, double price, boolean isVegetarian) {
         this.amount = amount;
         this.price = price;
         this.isVegetarian = isVegetarian;
